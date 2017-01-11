d8f7c4ee2ae8b21ceb6bab607bb7d278247fac65
sirokujira <toru753@gmail.com>
2017-01-11 17:29:11 +0900

    no use file delete

--- DeviceCode/Targets/OS/Toppers/hrp2/workspace/helloev3onnetmf/module_cfg.h
+++
@@ -1,6 +0,0 @@
-extern ID _module_id_APP_INIT_TASK;
-#define APP_INIT_TASK ((const ID)(_module_id_APP_INIT_TASK))
-
-extern ID _module_id_MAIN_TASK;
-#define MAIN_TASK ((const ID)(_module_id_MAIN_TASK))
-
