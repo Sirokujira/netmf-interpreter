a5978692e34df62c118decd38ab3d5b500c9cb1c
sirokujira <toru753@gmail.com>
2017-08-10 13:20:45 +0900

    proj file modified

---
+++ install-vsix-appveyor.ps1
@@ -0,0 +1,6 @@
+# This PS installs the nanoFramework Visual Studio Extension for VS2015
+
+$vsixPath = "$($env:USERPROFILE)\NetMFVS14.vsix"
+(New-Object Net.WebClient).DownloadFile('https://github.com/NETMF/netmf-interpreter/releases/download/v4.4-RTW-20-Oct-2015/NetMFVS14.vsix', $vsixPath)
+"`"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\VSIXInstaller.exe`" /q /a $vsixPath" | out-file ".\install-vsix.cmd" -Encoding ASCII
+& .\install-vsix.cmd
