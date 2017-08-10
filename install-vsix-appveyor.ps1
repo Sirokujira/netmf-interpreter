99ef210ef572196592bbad2a7b303a82a920ac2e
sirokujira <toru753@gmail.com>
2017-08-10 13:37:00 +0900

    appveyor modified

--- install-vsix-appveyor.ps1
+++ install-vsix-appveyor.ps1
@@ -2,5 +2,6 @@
 
 $vsixPath = "$($env:USERPROFILE)\NetMFVS14.vsix"
 (New-Object Net.WebClient).DownloadFile('https://github.com/NETMF/netmf-interpreter/releases/download/v4.4-RTW-20-Oct-2015/NetMFVS14.vsix', $vsixPath)
-"`"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\VSIXInstaller.exe`" /q /a $vsixPath" | out-file ".\install-vsix.cmd" -Encoding ASCII
+# "`"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\VSIXInstaller.exe`" /q /a $vsixPath" | out-file ".\install-vsix.cmd" -Encoding ASCII
+"`"C:\Program Files (x86)\Microsoft Visual Studio\2015\Community\Common7\IDE\VSIXInstaller.exe`" /q /a $vsixPath" | out-file ".\install-vsix.cmd" -Encoding ASCII
 & .\install-vsix.cmd
